2fb41087dfa57cfe8d0e1926279443c1bbe0402e
dethegeek <dethegeek@gmail.com>
2018-04-01 23:08:19 +0200

    fix interactivity and netbios domain

--- common/debian/samba/samba.sh
+++ common/debian/samba/samba.sh
@@ -8,5 +8,11 @@ installSamba()
 
 setupSamba()
 {
-    samba-tool domain provision --use-rfc2307 --interactive --function-level=2008_R2 --dns-backend=BIND9_DLZ --server-role=dc --realm=$UPPER_SAMBA_DOMAIN
+    mv /etc/samba/smb.conf /etc/samba.smb.conf.stock
+    samba-tool domain provision --use-rfc2307 --function-level=2008_R2 --dns-backend=BIND9_DLZ --server-role=dc --realm=$UPPER_SAMBA_DOMAIN --domain=$SAMBA_NETBIOS_DOMAIN
+}
+
+installDnsBackend()
+{
+    apt-get install -y bind9 dlz-ldap-enum
 }
